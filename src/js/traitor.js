afc1e98984c6c67916ed6451b7ebe6be3d9d01a2
Alexander Hems <alexanderhems@hotmail.com>
2016-01-10 15:18:59 +0100

    fix something

--- src/js/traitor.js
+++ src/js/traitor.js
@@ -22,6 +22,9 @@
       if(video){
           video.removeAttribute('src')
       }
-      document.write('<div style="position: fixed; z-index: -99; width: 100%; height: 100%"> <iframe frameborder="0" height="100%" width="100%" src="https://youtube.com/embed/lhK94idiqUY?autoplay=1&loop=1&controls=0&showinfo=0&autohide=1&playlist=lhK94idiqUY"></iframe></div>')
+      var body = document.querySelector('body');
+      body.style.margin = '0';
+      var traitor = '<div style="position: fixed; z-index: -99; width: 100%; height: 100%"> <iframe frameborder="0" height="100%" width="100%" src="https://youtube.com/embed/lhK94idiqUY?autoplay=1&loop=1&controls=0&showinfo=0&autohide=1&playlist=lhK94idiqUY"></iframe></div>';
+      body.appendChild(traitor);
       //document.querySelector('body').style.margin = '0';
   }
